5b12ecffa2e84bd70b0dfdbf2dd199734190c802
Sebastian <sebastian4j@gmail.com>
2018-04-20 07:48:42 -0300

    mas ejemplos

--- estaticos/src/main/java/com/sebastian/estaticos/Estaticos.java
+++ estaticos/src/main/java/com/sebastian/estaticos/Estaticos.java
@@ -6,11 +6,17 @@ package com.sebastian.estaticos;
  */
 public class Estaticos {
     interface Interface {
+        static void saludoEstatico() {
+            
+        }
         default void saludo() {
+//        default static void saludo() { <-- static o default
             System.out.println("hola desde la interface");
         }
     }
     static class Hola implements Interface {
+//        @Override <-- no se puede, no es de la instancia, es de la clase
+        public void saludoEstatico() { } // no es un override
         @Override
         public void saludo() {
 //        public static void saludo() {            
@@ -23,6 +29,8 @@ public class Estaticos {
     
     public static void main(String[] args) {
         new Hola().saludo();
+//        new Hola().saludoEstatico(); <-- error, no le pertenece a hola
+        Interface.saludoEstatico();
 //        new Chao().saludo(); <-- no se puede, no es estatica
         new Estaticos().new Chao().saludo();
     }
