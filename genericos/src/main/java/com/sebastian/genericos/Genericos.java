47bb65f0eb766bee0a127404836738da2eb65044
sebastian <sebastian@debian.casa>
2018-05-04 23:51:51 -0300

    agregando genericos

--- genericos/src/main/java/com/sebastian/genericos/Genericos.java
+++ genericos/src/main/java/com/sebastian/genericos/Genericos.java
@@ -8,22 +8,32 @@ import java.util.List;
  * @author sebastian
  */
 public class Genericos {
-    class A {}        
-    interface D {}
-    class B extends A {}
-    class C extends B implements D {}
-    
+
+    class A {
+    }
+
+    interface D {
+    }
+
+    class B extends A {
+        public void chao() {
+        }
+    }
+
+    class C extends B implements D {
+
+        public void saludar() {
+        }
+    }
+
     public static void main(String[] args) {
         new Genericos().validar();
     }
-    
+
     private void validar() {
         validarList();
     }
-    
-    
-    
-    
+
     private void validarList() {
 //        List<C> aa = new ArrayList<A>(); error, tiene que ser el mismo tipo
         List<C> bb = new ArrayList<C>(); // se puede omitir el C, B, D de la derecha
@@ -50,24 +60,26 @@ public class Genericos {
 //        mm.add(new Object()); error, solo D y supertipos
 //        mm.add(new B()); error, solo D y supertipos
 //        mm.add(new A()); error, solo D y supertipos
-        gg(new ArrayList<B>());
+        final List<B> bbb = new ArrayList<B>();
+        bbb.add(new B());
+        gg(bbb);
         gg(new ArrayList<A>());
         gg(new ArrayList<C>());
     }
-    
+
     private <T> List<T> aa(T t) {
         return new ArrayList<T>();
     }
-    
+
 //    private <T super A> List<T> bb(T t) {  error
-    private <T extends A> List<T> bb(T t) {        
+    private <T extends A> List<T> bb(T t) {
         return new ArrayList<T>();
     }
 //     no puede ir el ?, tiene que ser T
 /*    private <? extends A> List<T> cc(T t) { 
         return new ArrayList<T>();
     }
-*/
+     */
 //    no puede ser super
 //    private <T super C> void cc(T t) {
 //    private <? super C> void cc(T t) {        
@@ -80,18 +92,21 @@ public class Genericos {
 //        t.add(new T()); error
         return null;
     }
+
     private <T> D ee(List<? super T> t) {
 //        t.add(new T()); error
 //        t.add(new A()); error
         return null;
     }
-    
+
     private void gg(List<? super C> t) {
         t.add(new C());
-        t.stream().forEach(e -> System.out.println(e.getClass().getName()));
+        
+        t.stream().forEach(e -> {
+            System.out.println(e.getClass().getName());
+            System.out.println(e.toString());
+        });
 //        t.add(new A()); solo C
 //        t.add(new B()); solo C
     }
 }
-
-
